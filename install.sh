a8215bdc06c243c445a97f46f8d548d10b58d0cd
Courtney Shearer <csheare@clemson.edu>
2019-03-26 11:27:05 -0400

    Added emacs no new window

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ SCRIPTS="$PWD/scripts"
 echo "alias np='bash $SCRIPTS/py_temp.sh'" >> ~/.bash_profile
 echo "alias gi='bash $SCRIPTS/gitignore.sh'" >> ~/.bash_profile
 echo "alias f='bash $SCRIPTS/create_flask.sh'" >> ~/.bash_profile
+echo "alias emacs="emacs -nw"" >> ~/.bash_profile
+
